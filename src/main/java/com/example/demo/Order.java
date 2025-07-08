1c35abc0f51ce0e630655ed5a75728e08a51e3e5
Adria <adria.jorda.m@gmail.com>
2025-07-08 18:04:51 +0200

    [add] creado el objeto order con los get y set

--- src/main/java/com/example/demo/Order.java
+++ src/main/java/com/example/demo/Order.java
@@ -1,4 +1,34 @@
 package com.example.demo;
 
 public class Order {
+    private int id;
+    private String status;
+    private String reference;
+
+    // Getter y Setter para id
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter y Setter para status
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    // Getter y Setter para reference
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
 }
